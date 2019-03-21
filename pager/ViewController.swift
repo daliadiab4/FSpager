5d0064ae5b0978ff0b4299658f428a7c22f715df
Dalia Diab <admin@admins-macbook-pro.local>
2019-03-21 16:21:47 +0200

    Initial Commit

---
+++ pager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pager
+//
+//  Created by admin on 3/21/19.
+//  Copyright © 2019 DaliaDiab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
